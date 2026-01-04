dc764b1cd57ae13350a31c913f05cb6decdbb7b3
jongar <jongarcicuevas@gmail.com>
2026-01-04 17:21:03 +0100

    Fan PWM controller improved

--- scripts/fan_controller.py
+++ scripts/fan_controller.py
@@ -1,9 +1,9 @@
 import os
 import time
-from gpiozero import PWMOutputDevice
+import pigpio
 
 # CONFIGURATION
-GPIO_PIN = 14          
+PWM_PIN = 12         #Must be 12, 13, 18 o 19 for PWM hardware 
 TEMP_MIN = 45          
 TEMP_MAX = 70         
 PWM_FREQ = 25000        
@@ -13,7 +13,7 @@ CHECK_WAIT = 5
 DUTY_MIN = 0.66      # 0.66 = 3.3V  
 DUTY_MAX = 1.0
 
-fan = PWMOutputDevice(GPIO_PIN, frequency=PWM_FREQ)
+pi = pigpio.pi()
 
 def get_cpu_temp():
     res = os.popen('vcgencmd measure_temp').readline()
@@ -29,18 +29,18 @@ def calculate_speed(temp):
         speed = DUTY_MIN + (DUTY_MAX - DUTY_MIN) * ((temp - TEMP_MIN) / (TEMP_MAX - TEMP_MIN))
         return speed
 
-print(f"Control de ventilador activo en GPIO {GPIO_PIN}...")
+print(f"Control de ventilador activo en GPIO {PWM_PIN}...")
 
 try:
     while True:
         current_temp = get_cpu_temp()
         new_speed = calculate_speed(current_temp)
-        
-        fan.value = new_speed
+        duty= int(new_speed * 1000000)
+        pi.hardware_PWM(PWM_PIN, PWM_FREQ, duty)
         
         print(f"Temp: {current_temp}°C -> Speed (PWM): {new_speed:.2f}")
         time.sleep(CHECK_WAIT)
 
 except KeyboardInterrupt:
-    fan.off()
-    print("\nPrograma detenido.")
+    pi.hardware_PWM(PWM_PIN, PWM_FREQ, 0)
+    pi.stop()
